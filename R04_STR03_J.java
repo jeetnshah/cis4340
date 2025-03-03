aa52573cbadd59452b839a863791d632150fda58
Jeet Shah <je708473@ucf.edu>
2025-03-02 21:07:54 -0500

    FIxed unsafe string encoding issue

--- R04_STR03_J.java
+++ R04_STR03_J.java
@@ -2,16 +2,29 @@ import java.math.BigInteger;
 import java.util.Arrays;
 
 public class R04_STR03_J {
+
+    // Non-Compliant Version
+    public static BigInteger nonCompliant(BigInteger x) {
+        byte[] byteArray = x.toByteArray();
+        String s = new String(byteArray);
+        byteArray = s.getBytes();
+        return new BigInteger(byteArray);
+    }
+
+    // Compliant Version
+    public static BigInteger compliant(BigInteger x) {
+        byte[] byteArray = x.toByteArray();
+        byte[] copiedArray = Arrays.copyOf(byteArray, byteArray.length);
+        return new BigInteger(copiedArray);
+    }
+
     public static void main(String[] args) {
         BigInteger x = new BigInteger("530500452766");
-        
-        // Secure method: Keep bytes as raw data
-        byte[] byteArray = x.toByteArray();
-        
-        // If needed, encode as Base64 or Hex instead of String conversion
-        byte[] copiedArray = Arrays.copyOf(byteArray, byteArray.length);
-        
-        BigInteger newX = new BigInteger(copiedArray);
-        System.out.println("Reconstructed value: " + newX);
+
+        System.out.println("Running Non-Compliant Code:");
+        System.out.println(nonCompliant(x));
+
+        System.out.println("\nRunning Fixed Code:");
+        System.out.println(compliant(x));
     }
 }
