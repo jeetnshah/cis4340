014fe13e9ffbbee6289fbc0ee1f68c0e7c278dee
Jeet Shah <je708473@ucf.edu>
2025-03-02 21:12:15 -0500

    Uses the synchronized keyword to prevent multiple threads from modifying counter at the same time

--- R12_THI00_J.java
+++ R12_THI00_J.java
@@ -1,7 +1,23 @@
 public class R12_THI00_J {
     private static int counter = 0;
 
-    public synchronized static void increment() {
+    // Non-Compliant Version
+    public static void nonCompliant() {
+        counter++; // No synchronization, unsafe in multithreaded environments
+    }
+
+    // Compliant Version
+    public synchronized static void compliant() {
         counter++; // Now thread-safe
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Running Non-Compliant Code:");
+        nonCompliant();
+        System.out.println("Counter: " + counter);
+
+        System.out.println("\nRunning Fixed Code:");
+        compliant();
+        System.out.println("Counter: " + counter);
     }
 }
