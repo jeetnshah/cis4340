9ee478c42cb25fcb7d2437a2e19152f3e2878c92
Jeet Shah <je708473@ucf.edu>
2025-03-02 21:05:59 -0500

    Fixed issue with ignoring return values

--- R02_XP00_J.java
+++ R02_XP00_J.java
@@ -1,17 +1,28 @@
 import java.io.File;
 
 public class R02_XP00_J {
-    public static void main(String[] args) {
-        deleteFile();
+
+    // Non-Compliant Version
+    public static void nonCompliant() {
+        File someFile = new File("someFileName.txt");
+        someFile.delete();
     }
 
-    public static void deleteFile() {
+    // Compliant Version
+    public static void compliant() {
         File someFile = new File("someFileName.txt");
-
         if (!someFile.delete()) {
             System.err.println("Error: Failed to delete the file.");
         } else {
             System.out.println("File deleted successfully.");
         }
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Running Non-Compliant Code:");
+        nonCompliant();
+
+        System.out.println("\nRunning Fixed Code:");
+        compliant();
     }
 }
