68eab5bc100a3095d0c2e02843882a24f63393df
Jeet Shah <je708473@ucf.edu>
2025-03-02 21:02:43 -0500

    Added Security fix for logging unsanitzed user input

--- R00_IDS03_J.java
+++ R00_IDS03_J.java
@@ -4,10 +4,17 @@ import java.util.regex.Pattern;
 public class R00_IDS03_J {
     private static final Logger logger = Logger.getLogger(R00_IDS03_J.class.getName());
 
-    public static void main(String[] args) {
-        String username = "admin";  // Simulated user input
-        boolean loginSuccessful = false;  // Simulated login failure
+    // Non-Compliant Version
+    public static void nonCompliant(String username, boolean loginSuccessful) {
+        if (loginSuccessful) {
+            logger.severe("User login succeeded for: " + username);
+        } else {
+            logger.severe("User login failed for: " + username);
+        }
+    }
 
+    // Compliant Version
+    public static void compliant(String username, boolean loginSuccessful) {
         if (loginSuccessful) {
             logger.severe("User login succeeded for: " + sanitizeUser(username));
         } else {
@@ -17,5 +24,14 @@ public class R00_IDS03_J {
 
     public static String sanitizeUser(String username) {
         return Pattern.matches("[A-Za-z0-9]+", username) ? username : "unauthorized user";
+    }
+
+    public static void main(String[] args) {
+        String username = "admin123!@#";
+        System.out.println("Running Non-Compliant Code:");
+        nonCompliant(username, false);
+
+        System.out.println("\nRunning Fixed Code:");
+        compliant(username, false);
     }
 }
