6f2d09c598536f3fe6ddb7e65f3ce25676d4fcf7
Jeet Shah <je708473@ucf.edu>
2025-03-02 21:12:59 -0500

    Retrieves sensitive information fromm environment variables

--- R13_FIO00_J.java
+++ R13_FIO00_J.java
@@ -1,10 +1,26 @@
 public class R13_FIO00_J {
-    public static void main(String[] args) {
+
+    // Non-Compliant Version
+    public static void nonCompliant() {
+        String password = "SuperSecret123"; // Hardcoded password
+        System.out.println("Using password: " + password);
+    }
+
+    // Compliant Version
+    public static void compliant() {
         String password = System.getenv("APP_PASSWORD");
         if (password == null) {
             System.err.println("Error: No password set in environment variables.");
         } else {
             System.out.println("Password retrieved securely.");
         }
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Running Non-Compliant Code:");
+        nonCompliant();
+
+        System.out.println("\nRunning Fixed Code:");
+        compliant();
     }
 }
