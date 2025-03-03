0f3505889b1e4db8a9597288baa4ae9f820a3ba2
Jeet Shah <je708473@ucf.edu>
2025-03-02 21:10:23 -0500

    Automatically closes the resource

--- R07_ERR00_J.java
+++ R07_ERR00_J.java
@@ -1,11 +1,25 @@
 import java.io.*;
 
 public class R07_ERR00_J {
-    public static void main(String[] args) {
+
+    // Non-Compliant Version
+    public static void nonCompliant() throws IOException {
+        BufferedReader reader = new BufferedReader(new FileReader("file.txt"));
+        System.out.println(reader.readLine());
+        // No call to reader.close(), leading to a resource leak
+    }
+
+    // Compliant Version
+    public static void compliant() {
         try (BufferedReader reader = new BufferedReader(new FileReader("file.txt"))) {
             System.out.println(reader.readLine());
         } catch (IOException e) {
             System.err.println("Error reading file: " + e.getMessage());
         }
     }
-}
+
+    public static void main(String[] args) {
+        System.out.println("Running Non-Compliant Code:");
+        try {
+            nonCompliant();
+        } catch (IOException e) {
