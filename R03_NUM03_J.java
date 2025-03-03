aa3055698fdd657acaec17c8635b69faed66c4b0
Jeet Shah <je708473@ucf.edu>
2025-03-02 21:07:03 -0500

    Fixed interger representation issue

--- R03_NUM03_J.java
+++ R03_NUM03_J.java
@@ -3,15 +3,27 @@ import java.io.ByteArrayInputStream;
 import java.io.IOException;
 
 public class R03_NUM03_J {
-    public static void main(String[] args) throws IOException {
-        byte[] inputData = { 0, 0, 0, 127 }; // Example data
-        DataInputStream dis = new DataInputStream(new ByteArrayInputStream(inputData));
 
-        long result = getUnsignedInteger(dis);
-        System.out.println("Result: " + result);
+    // Non-Compliant Version
+    public static int nonCompliant(DataInputStream is) throws IOException {
+        return is.readInt();
     }
 
-    public static long getUnsignedInteger(DataInputStream is) throws IOException {
-        return is.readInt() & 0xFFFFFFFFL; // Mask with 32 one-bits to ensure unsigned interpretation
+    // Compliant Version
+    public static long compliant(DataInputStream is) throws IOException {
+        return is.readInt() & 0xFFFFFFFFL;
+    }
+
+    public static void main(String[] args) throws IOException {
+        byte[] inputData = { 0, 0, 0, 127 };
+        DataInputStream dis = new DataInputStream(new ByteArrayInputStream(inputData));
+
+        System.out.println("Running Non-Compliant Code:");
+        System.out.println(nonCompliant(dis));
+
+        dis = new DataInputStream(new ByteArrayInputStream(inputData));
+
+        System.out.println("\nRunning Fixed Code:");
+        System.out.println(compliant(dis));
     }
 }
